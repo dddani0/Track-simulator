9d595a2658b6012c072afdf713e65e930b45e934
marley <49081673+marloss@users.noreply.github.com>
2021-11-06 18:47:07 +0100

    Initial commit
    Setup commit

---
+++ Assets/Car_Behavior.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car_Behavior : MonoBehaviour
+{
+    //Basic information:
+    //This script uses rear wheels to provide the drive.
+    //The game will be heavy due to physics implemented.
+    //We also use metric system (Kilometers, Meters ...)
+    //The car needs the following forces to control the acceleration and deacceleration of the car (speed).
+    //The longitude force and Lateral force are separate.
+    //The car also needs angular momentum (perdület), collision and other forces which influence the car physics.
+
+    //HANDLE: Longitude force
+    //-> Lognitude force is the force, which moves the car forward and backwards
+    //-> Important to keep in mind, that if the car moves forward, the wheels push backwards!
+    //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    //-> So like: Car moves forward: Wheel = -force something like that
+    //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+    //HANDLE: Lateral force
+    //-> Allows the car to turn, caused by the sideway friction of the wheels.
+
+    //HANDLE: straight line physics
+    //-> Tractive force: force delivired by the engine via rear wheels: rotation wise, it turns the wheels forward.
+    //-> The wheels push against the opposite direction of the intended direction, and the surface pushes the wheels back, resulting in moving
+    //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    //-> Formulatic way: F (traction) = unit vector (PlayerInput) * Engineforce
+    //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    //-> To fight against infinite acceleration, we have calculate resistant forces (Air resistance, rolling resistance).
+    //-> Air resistance is one of the most important resistance force, when we're talking about high speed car drive. It's proportional to the square of the velocity.
+    //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    //-> Formulatic way: F (aerodynamic drag) = -C (constant drag) * v (velocity vector) * v.magnitude (speed)
+    //-> IMPORTANT: Since we're talking about square of velocity, we have to: sqrt( var.x * var.x + v.y * v.y)
+    //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    //-> Rolling resistance is the friction between the surface and the tire.
+    //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    //-> Formulatic way: F (rolling resistance) = -C (constant rolling resistance) * v (speed)
+    //-> Constant rolling resistance must be > constant aerodynamic drag * 30
+    //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    //-> Longitudal force is the sum of these forces like:
+    //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    //-> F (longitudal) = F (traction) + F (aerodynmic drag) + F (rolling resistance)
+    //-> Since it's resistant forces, we're basically subtracting these forces from the traction force
+    //-> When the vehicle is in stall, the vehicle is force is zero!
+    //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    //-> Car's velocity is calculated using Newton's second law
+    //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    //-> Formulatic way: a (acceleration) = F / M (car's mass in kilogram)
+    //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
